7704b04e47072e34f80fd72289f7599c1429d23d
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2025-05-16 07:38:23 -0400

    trying minify style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	build: {
+		inlineStylesheets: "always", // Use 'always' to bundle all CSS into a single file
+	},
+	vite: {
+		build: {
+			cssCodeSplit: false, // Disable CSS code splitting
+			minify: true, // Enable minification for all assets
+		},
+	},
+});
